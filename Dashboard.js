d2c4b453312ebe38be0e17660e611eae99f739e6
EmilySamigramni <105202240+EmilySamigramni@users.noreply.github.com>
2024-09-15 21:38:18 +0200

    Add files via upload

---
+++ Dashboard.js
@@ -0,0 +1,13 @@
+// src/components/Dashboard.js
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Welcome to your dashboard! You are now logged in.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
